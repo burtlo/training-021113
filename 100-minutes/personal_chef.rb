22c2b7dc30e8d59bca21932c95869546660640e0
Franklin Webber <franklin.webber@gmail.com>
2013-02-11 13:08:53 -0700

    #good_morning method

--- 100-minutes/personal_chef.rb
+++ 100-minutes/personal_chef.rb
@@ -1,8 +1,41 @@
+require 'date'
+
 class PersonalChef
-  def make_toast
-    puts "Making your toast!"
+  def make_toast(style = default_toast_style)
+    puts "Making your #{style} toast!"
+    self
+  end
+
+  def make_milkshake
+    puts "Make a milkshakes"
+    self
+  end
+
+  def default_toast_style
+    "sourdough"
+  end
+
+end
+
+class Butler
+  def open_front_door
+    puts "Opening Front Door, SIRRRRRR!"
+  end
+  
+  def good_morning
+    date = Date.today
+    day_of_week = date.strftime "%A"
+    day_of_year = date.strftime "%j"
+    year = date.strftime "%Y"
+    year = Time.now.year.to_s
+    
+    puts "Happy #{day_of_week}, it's the #{day_of_year} day of #{year}"
   end
 end
 
 frank = PersonalChef.new
-frank.make_toast
+frank.make_toast.make_milkshake
+
+jeeves = Butler.new
+jeeves.open_front_door
+jeeves.good_morning
