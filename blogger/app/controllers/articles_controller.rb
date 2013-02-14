e0f4169911699b013b0f8b24f77bcad761b81183
Franklin Webber <franklin.webber@gmail.com>
2013-02-14 14:34:18 -0700

    Comment Model created

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -28,7 +28,15 @@ class ArticlesController < ApplicationController
     # An alternative to updating the article
     # Article.update @article, params[:article]
 
+    flash[:message] = "You're awesome, you updated the article"
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:message] = "You're awesome, You deleted article '#{@article.title}'"
+    redirect_to articles_path
   end
 
 end
