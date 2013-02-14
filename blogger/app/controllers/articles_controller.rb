7744f14da8a9f5f753c16b40defdfb8e8f96e52a
Franklin Webber <franklin.webber@gmail.com>
2013-02-14 12:02:49 -0700

    Blogger - Article (index,show,create,update,edit)

---
+++ blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,34 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create params[:article]
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes params[:article]
+
+    # An alternative to updating the article
+    # Article.update @article, params[:article]
+
+    redirect_to article_path(@article)
+  end
+
+end
---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '6374648eabea7132a3c72e121842f014cb55c87683d6a9036dc67177627ac5ba9995d02cbfa1aaa2bfe71180a8b6f13f596877ac5e6f26cde1ef806a72066423'
