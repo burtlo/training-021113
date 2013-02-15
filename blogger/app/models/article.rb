19d151b21af9815cee45b0aa05556d07f279a4fc
Franklin Webber <franklin.webber@gmail.com>
2013-02-15 10:18:40 -0700

    Taggings

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,6 +1,25 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :tag_list
+  validates :title, :body, presence: true
+
   has_many :comments
 
-  validates :title, :body, presence: true
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  def tag_list
+    tags.collect { |tag| tag.name }.join(", ")
+  end
+
+  def tag_list=(value)
+    tag_names = tag_list_to_tag_names(value)
+    found_tags = tag_names.map { |name| Tag.find_or_create_by_name name }
+    self.tags = found_tags
+  end
+
+  def tag_list_to_tag_names(value)
+    value.split(",").map {|name| name.strip.downcase }
+  end
+
 end
---
+++ blogger/app/models/tag.rb
@@ -0,0 +1,6 @@
+class Tag < ActiveRecord::Base
+  attr_accessible :name
+  
+  has_many :taggings
+  has_many :articles, through: :taggings
+end
